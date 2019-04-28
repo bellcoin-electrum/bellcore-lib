4b361030c7e2bff9c4e01e8226e6fd9353c8c737
y-chan <yuto_tetuota@yahoo.co.jp>
2019-04-28 11:43:23 +0900

    bellcore-lib

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ var bitcore = module.exports;
 bitcore.version = 'v' + require('./package.json').version;
 bitcore.versionGuard = function(version) {
   if (version !== undefined) {
-    var message = 'More than one instance of bitcore-lib found. ' +
-      'Please make sure to require bitcore-lib and check that submodules do' +
-      ' not also include their own bitcore-lib dependency.';
+    var message = 'More than one instance of bellcore-lib found. ' +
+      'Please make sure to require bellcore-lib and check that submodules do' +
+      ' not also include their own bellcore-lib dependency.';
     throw new Error(message);
   }
 };
-bitcore.versionGuard(global._bitcore);
-global._bitcore = bitcore.version;
+bitcore.versionGuard(global._bellcore);
+global._bellcore = bitcore.version;
 
 // crypto
 bitcore.crypto = {};
